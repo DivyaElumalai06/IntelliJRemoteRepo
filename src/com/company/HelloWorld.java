734196469a966e9037c91133c4ee9936d3c48164
DivyaElumalai06 <divya_elumalai@yahoo.com>
2020-04-28 15:52:22 +1000

    Added changes

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -3,7 +3,7 @@ package com.company;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hi");
+        System.out.println("Hello World");
 
     }
 
